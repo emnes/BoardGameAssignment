4d68e6e59ffa2141d6cd553a297c971fb8fdf97e
m.shar <mazshar@hotmail.co.uk>
2014-10-28 09:28:57 -0400

    adding annotations

---
+++ BoardGame/BoardGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BoardGame
+//
+//  Created by Mazhar Shar on 28/10/2014.
+//  Copyright (c) 2014 Mazhar Shar. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
