0cacc1d417330e0b1048dcf2edcaaf33084bee6a
m.shar <mazshar@hotmail.co.uk>
2014-11-12 18:09:52 -0500

    Minor changes, reverted inheritance - going to ask TA tomorrow about this

--- src/Tile.h
+++ src/Tile.h
@@ -14,111 +14,132 @@
 
 using namespace std;
 
-enum Goods{SPICE, FABRIC, JEWEL, RUBY};
+enum Goods
+{
+    SPICE,
+    FABRIC,
+    JEWEL,
+    RUBY
+};
 
 template<typename J> 
-class Tile{
-	
+class Tile
+{
 	private:
 		vector<J> players;
 		
 	public:
-		//bool operation==(const Tile &t);
+		//bool operator==(const Tile &t);
 		virtual bool action( Player& player){return false;}
 		//virtual Tile* clone();
-		//ostream& operation<<;
+		//ostream& operator<<;
 		vector<J> getPlayers(){return players;}
 };
 
+// Desert has same function as base but should still exist, I think.
+
+/*template<typename J>
+class Desert : public Tile<J>
+{
+    public:
+}; */
+
 template<typename J>
-class Restaurant : public Tile<J>{
-	
-	public:	
+class Restaurant : public Tile<J>
+{
+	public:
 		virtual bool action( Player& player) override;
 };
 
 template<typename J>
-class SpiceMerchant : public Tile<J>{
-	
+class Casino : public Tile<J>
+{
+    public:
+        Casino(){srand(time(NULL));}
+        bool action( Player& player) override;
+};
+
+template<typename J>
+class Palace : public Tile<J>
+{
+    public:
+        bool action( Player& player) override;
+};
+
+template<typename J>
+class Jeweler : public Tile<J>
+{
+    public:
+        bool action( Player& player) override;
+};
+
+// Speak to TA about using inheritance for Manufacturers, Merchants and Markets.
+//--------- Manufacturers ---------\\
+
+template<typename J>
+class FabricManufacturer : public Tile<J>
+{
 	public:	
 		bool action( Player& player) override;
 };
 
 template<typename J>
-class FabricManufacturer : public Tile<J>{
-	
+class CartManufacturer : public Tile<J>
+{
+    public:
+        bool action( Player& player) override;
+};
+
+//--------- Markets ---------\\
+
+template<typename J>
+class SmallMarket : public Tile<J>
+{
 	public:	
 		bool action( Player& player) override;
 };
 
 template<typename J>
-class Jeweler : public Tile<J>{
-	
-	public:	
-		bool action( Player& player) override;
-};
-
-template<typename J>
-class CartManufacturer : public Tile<J>{
-	
+class SpiceMarket : public Tile<J>
+{
 	public:
 		bool action( Player& player) override;
 };
 
 template<typename J>
-class SmallMarket : public Tile<J>{
-	
-	public:	
+class JewelryMarket : public Tile<J>
+{
+    public:
 		bool action( Player& player) override;
 };
 
 template<typename J>
-class SpiceMarket : public Tile<J>{
-	
+class FabricMarket : public Tile<J>
+{
 	public:
 		bool action( Player& player) override;
 };
 
 template<typename J>
-class JewelryMarket : public Tile<J>{
-	
-	public:
-		bool action( Player& player) override;
-};
-
-template<typename J>
-class FabricMarket : public Tile<J>{
-	
-	public:
-		bool action( Player& player) override;
-};
-
-template<typename J>
-class BlackMarket : public Tile<J>{
-	
+class BlackMarket : public Tile<J>
+{
 	public:
 		BlackMarket(){srand(time(NULL));}
 		bool action( Player& player) override;
 };
 
+//--------- Merchants ---------\\
+
 template<typename J>
-class Casino : public Tile<J>{
-	
-	public:
-		Casino(){srand(time(NULL));}
-		bool action( Player& player) override;
+class SpiceMerchant : public Tile<J>
+{
+    public:
+        bool action(Player& player) override;
 };
 
 template<typename J>
-class GemMerchant : public Tile<J>{
-	
-	public:
-		bool action( Player& player) override;
-};
-
-template<typename J>
-class Palace : public Tile<J>{
-	
+class GemMerchant : public Tile<J>
+{
 	public:
 		bool action( Player& player) override;
 };
