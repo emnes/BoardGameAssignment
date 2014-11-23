f1383d65d7889e26ff41d7424dd911ca54cb4d64
m.shar <mazshar@hotmail.co.uk>
2014-11-22 22:04:31 -0500

    Added comments

--- src/GameBoard.h
+++ src/GameBoard.h
@@ -2,6 +2,12 @@
 //  GameBoard.h
 //  BoardGame
 //
+//  Parameterized class (template) for the game board which acts as a grid builder.
+//  The tiles of the grid are of type T and hold one or more players of type J.
+//  Game board has N players.
+//
+//  Tiles are indentified by row and col. Players identified by name.
+//
 //  Created by Mazhar Shar & Patrice Boulet on 28/10/2014.
 //  Copyright (c) 2014 Mazhar Shar & Patrice Boulet. All rights reserved.
 //
@@ -16,26 +22,26 @@
 #include "Tile.h"
 
 using namespace std;
-enum Move{TOP, DOWN, LEFT, RIGHT};
+enum Move{TOP, DOWN, LEFT, RIGHT}; // Only four possible neighbours: up, down, left, right
+
 
 template<typename T, typename J, unsigned N, size_t ROW, size_t COL> class GameBoard {
 	
-	private:
-		array<array<T, ROW>, COL> board;
-		map<string, J*> players;
+private:
+	array<array<T, ROW>, COL> board;
+	map<string, J*> players;
 		
-	public:
-		GameBoard();
-		void add(const T& tile, int row, int col);
-		const T& getTile(int row, int col) const;
-		//TODO: implement      void getCoordinate(const T &tile, int *row, int *col) const;
-		//TODO: implement      void setPlayer(J player);
-		J getPlayer(const std::string& playerName);
-		//TODO: implement      const T& getTile(const std::string& playerName) const;
-		std::vector<J> getPlayers(const T& tile) const;
-		//TODO: implement      const T& move(Enum Move move, const std::string& playerName );
-		void addPlayer(string playerName);
-		
+public:
+	GameBoard();
+	void add(const T& tile, int row, int col);
+	const T& getTile(int row, int col) const;
+	//TODO: implement      void getCoordinate(const T &tile, int *row, int *col) const;
+	//TODO: implement      void setPlayer(J player);
+	J getPlayer(const std::string& playerName);
+	//TODO: implement      const T& getTile(const std::string& playerName) const;
+	std::vector<J> getPlayers(const T& tile) const;
+	//TODO: implement      const T& move(Enum Move move, const std::string& playerName );
+	void addPlayer(string playerName);
 };
 
 #endif /* defined(__BoardGame__GameBoard__) */
